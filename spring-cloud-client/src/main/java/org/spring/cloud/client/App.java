164bd545df6d02e36c0d74349e9a2a327c12926c
张俊伟 <zhangjunwei@wdb007.com>
2017-12-11 08:56:01 +0800

    客户端服务端配置

--- spring-cloud-client/src/main/java/org/spring/cloud/client/App.java
+++ spring-cloud-client/src/main/java/org/spring/cloud/client/App.java
@@ -1,19 +1,31 @@
 package org.spring.cloud.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  *
  */
-@EnableEurekaServer
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	SpringApplication.run(App.class, args);
-    }
+@RestController
+@EnableEurekaClient//注解表明是Eureka的client
+@RefreshScope
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Value("${hello}")
+	String hello;
+	@RequestMapping(value = "/hello")
+	public String hello() {
+		System.out.println("App.hello()");
+		return hello;
+	}
 }
